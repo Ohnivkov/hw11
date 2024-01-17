305d2f7965a4445ae3146364893430854594f5af
Ohnivkov <136961385+Ohnivkov@users.noreply.github.com>
2024-01-17 17:21:20 +0200

    new_program

---
+++ src/schemas.py
@@ -0,0 +1,29 @@
+from datetime import datetime, date
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class ContactModel(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+    phone: str
+    description: str
+    birth_date: date
+    created_at: datetime
+    updated_at: datetime
+
+
+class ResponseContactModel(BaseModel):
+    id: int = Field(default=1, ge=1)
+    name: str
+    surname: str
+    email: EmailStr
+    phone: str
+    description: str | None
+    birth_date: date
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
